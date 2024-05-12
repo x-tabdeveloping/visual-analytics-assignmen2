026ad4a100b96f647aa0ddf822f17e58e4661a08
Márton Kardos <power.up1163@gmail.com>
2024-05-12 14:15:26 +0200

    Abstracted data loading, added main functions

--- src/neural_network.py
+++ src/neural_network.py
@@ -2,44 +2,44 @@ from pathlib import Path
 
 import numpy as np
 import plotly.express as px
-from PIL import Image
 from sklearn.metrics import classification_report
 from sklearn.neural_network import MLPClassifier
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-from tensorflow.keras.datasets import cifar10
 
-from utils import class_names, flatten_images, greyscale_images
+from utils import load_data
 
-print("Loading and preprocessing data.")
-(X_train, y_train), (X_test, y_test) = cifar10.load_data()
-X_train = greyscale_images(X_train)
-X_test = greyscale_images(X_test)
-X_train = flatten_images(X_train)
-X_test = flatten_images(X_test)
-y_train = np.ravel(y_train)
-y_test = np.ravel(y_test)
-y_train = [class_names[y] for y in y_train]
-y_test = [class_names[y] for y in y_test]
 
-print("Training Neural Network classifier.")
-classifier = make_pipeline(StandardScaler(), MLPClassifier(random_state=42))
-classifier.fit(X_train, y_train)
+def main():
+    print("Loading and preprocessing data.")
+    X_train, y_train, X_test, y_test = load_data()
 
-print("Evaluating.")
-report = classification_report(y_test, classifier.predict(X_test))
-print(report)
+    print("Training Neural Network classifier.")
+    classifier = make_pipeline(
+        StandardScaler(), MLPClassifier((50, 50), max_iter=300, random_state=42)
+    )
+    classifier.fit(X_train, y_train)
 
-print("Saving report.")
-out_path = Path("out")
-out_path.mkdir(exist_ok=True)
-with open(out_path.joinpath("neural_network_report.txt"), "w") as report_file:
-    report_file.write(report)
+    print("Evaluating.")
+    report = classification_report(y_test, classifier.predict(X_test))
+    print(report)
 
-print("Producing loss curve")
-_, network = classifier.steps[1]
-fig = px.line(
-    x=np.arange(len(network.loss_curve_)), y=network.loss_curve_, title="Loss curve."
-)
-fig.update_layout(xaxis_title="Iteration", yaxis_title="Loss")
-fig.write_image(out_path.joinpath("neural_network_loss_curve.png"))
+    print("Saving report.")
+    out_path = Path("out")
+    out_path.mkdir(exist_ok=True)
+    with open(out_path.joinpath("neural_network_report.txt"), "w") as report_file:
+        report_file.write(report)
+
+    print("Producing loss curve")
+    _, network = classifier.steps[1]
+    fig = px.line(
+        x=np.arange(len(network.loss_curve_)),
+        y=network.loss_curve_,
+        title="Loss curve.",
+    )
+    fig.update_layout(xaxis_title="Iteration", yaxis_title="Loss")
+    fig.write_image(out_path.joinpath("neural_network_loss_curve.png"))
+
+
+if __name__ == "__main__":
+    main()
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+from tensorflow.keras.datasets import cifar10
 
 class_names = [
     "aquatic",
@@ -26,6 +27,7 @@ class_names = [
 
 
 def greyscale_images(images: np.ndarray) -> np.ndarray:
+    """Greyscales images in the array."""
     res = []
     for image in images:
         image = Image.fromarray(image)
@@ -34,5 +36,16 @@ def greyscale_images(images: np.ndarray) -> np.ndarray:
 
 
 def flatten_images(images: np.ndarray) -> np.ndarray:
+    """Flattens images in the given image array."""
     n, w, h = images.shape
     return np.reshape(images, (n, w * h))
+
+
+def load_data() -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """Loads CIFAR-10 data, greyscales and flattens images."""
+    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+    X_train = flatten_images(greyscale_images(X_train))
+    X_test = flatten_images(greyscale_images(X_test))
+    y_train = np.array([class_names[y] for y in np.ravel(y_train)])
+    y_test = np.array([class_names[y] for y in np.ravel(y_test)])
+    return X_train, y_train, X_test, y_test
